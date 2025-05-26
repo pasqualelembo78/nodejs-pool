35638cefcf77ae84c57a27d6ff113bb5980b74ce
MoneroOcean <support@moneroocean.stream>
2025-05-25 22:56:33 -0700

    New script

---
+++ manage_scripts/get_last_block.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const argv = require('minimist')(process.argv.slice(2));
+
+if (!argv.port) {
+        console.error("Please specify port");
+        process.exit(1);
+}
+const port = argv.port;
+
+require("../init_mini.js").init(function() {
+  global.coinFuncs.getPortLastBlockHeader(port, function (err_header, body_header) {
+    console.log("err:"  + JSON.stringify(err_header));
+    console.log("body:" + JSON.stringify(body_header));
+    process.exit(0);
+  });
+});
